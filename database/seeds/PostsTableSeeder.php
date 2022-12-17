41cfd5b9f10ac73b7567c51df1aa5a62220d62cf
DESKTOP-GUVHBIK\attom <921aaa818@gmail.com>
2022-12-17 16:21:47 +0900

    Posttableのシーディング（多分エラーなので確認）

---
+++ database/seeds/PostsTableSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class PostsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        DB::table('posts')->insert([
+            ['post' => 'つぶやいた内容を表示します。'],
+        ]);
+    }
+}
