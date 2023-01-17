ef86373a1518c19b7c576b45e443a2845f2a01b6
DESKTOP-GUVHBIK\attom <921aaa818@gmail.com>
2023-01-17 16:58:06 +0900

    投稿フォームの設置

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -14,48 +14,41 @@
           <textarea class="form-control" name="post" style="border:none" rows="4" required placeholder="何をつぶやこうか…?"></textarea>
         </div>
         <div class="form-send-icon">
-          <input type="image" src="images/post.png"  alt="投稿">
+          <button type="submit"><img src="images/post.png" alt="投稿"></button>
         </div>
       </div>
     </form>
 
-  <!-- 4.2 ログインユーザーのつぶやきを表示 -->
-  <!-- 4.2.1 ログインユーザーのフォローのつぶやき表示を表示 -->
-  @if (isset($timelines))
-  @foreach ($timelines as $timeline)
+<!-- 4.2.1 ユーザーのつぶやきを表示 -->
+@foreach ($timelines as $timeline)
   <div class="tweets-top">
-    <div class="card">
-      <div class="tweet-timelines">
-        <div id="top-image2" class="top-image2">
-
-          <p><img src="images/dawn.png" class="rounded-circle"></p>
+    <div class="timelines">
+      <div class="user-post">
+        <div class="post-image">
+          <img src="images/dawn.png" class="form-icon" alt="ユーザーアイコン">
         </div>
         <div class="timelines">
-          <p class="tweets-top-username">{{ $timeline->user->username }}</p>
-          <p class="tweets-top-text">{!! nl2br(e($timeline->posts)) !!}</p>
+          <p class="post-username">{{ $timeline->user->username }}</p>
+          <p class="post-text">{{ $timeline->posts }}</p>
         </div>
-        <div class="tweets-top-time">
+        <div class="post-time">
           <p>{{ $timeline->created_at }}</p>
         </div>
       </div>
 
-
-      @if ($timeline->id === Auth::user()->id)
-
-
-      <div class="tweet-menu">
-
-
-
-
-
+    <!-- 4.2.2 ログインユーザーのつぶやきを表示 -->
+    @if ($timeline->id === Auth::user()->id)
+      <div class="user-post">
+        <div class="edit">
+          <input type="image" src="/images/edit.png" alt="編集">
+           <!-- モーダルの設定 -->
+        </div>
+        <div class="delete">
+          <a class="btn btn-danger" href="/post/{{ $post->id }}/delete" img src="/images/trash.png" alt="削除" onclick="return confirm('削除しますか？')">削除</a>
+        </div>
       </div>
-
-      @endif
-
-    </div>
+    @endif
   </div>
-  @endforeach
-  @endif
 </div>
+@endforeach
 @endsection
