2297e2ec2467edf98f959656c12098b386ce583b
DESKTOP-GUVHBIK\attom <921aaa818@gmail.com>
2023-01-21 14:01:14 +0900

    トップ画面の投稿/タイムラインの編集と削除の実装

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -14,32 +14,51 @@
   </div>
 </div>
 
-  <!-- 4.2.1 ユーザーのつぶやきを表示 -->
+<!-- 4.2 ユーザーのつぶやきを表示 -->
+<div class="timeline">
   @foreach ($timelines as $timeline)
-  <div class="posts-top">
-    <div class="timelines">
-      <div class="user-post">
-        <div class="post-image">
-          <img  src="images/dawn.png" class="form-icon" alt="ユーザーアイコン">
-        </div>
-        <div class="post-content">
-          <p class="post-username">{{ $timeline->user->username }}</p>
-          <p class="post-time">{{ $timeline->created_at }}</p>
-          <p class="post-text">{{ $timeline->posts }}</p>
-    <!-- 4.2.2 ログインユーザーのボタン表示 -->
-            @if ($post->id === Auth::user()->id)
-            <div class="post-button">
-              <div class="edit">
-                <button type="button" class="modal-open edit-btn" data-toggle="modal" data-target="#editModal" data-id="{{ $timeline->id }}" data-posts="{{ $timeline->posts }}"><img src="storage/edit.png" alt="編集"></button>
-              </div>
-              <div class="delete">
-                <a class="delete-btn" href="/post/{{ $timeline->id }}/delete" onclick="return confirm('このつぶやきを削除します。よろしいですか？')"><img src="storage/trash_h.png" alt="削除"></a>
+  <div class="user-post">
+    @if($auths->id === $timeLine->user->id)
+    <a href="/profile">
+      <img class="user-post-icon" src="images/{{ $timeLine -> user -> images}}" alt="ユーザーアイコン">
+    </a>
+    @else
+    <a href="/profile/{{ $timeLine -> user_id}}">
+      <img class="user-post-icon" src="images/{{ $timeLine -> user -> images}}" alt="ユーザーアイコン">
+    </a>
+    @endif
+      <p class="post-username">{{ $timeline->user->username }}</p>
+      <p class="post-time">{{ $timeline->created_at }}</p>
+      <p class="post-text">{{ $timeline->posts }}</p>
+
+    <!-- 4.3 ログインユーザーのボタン表示 -->
+    @if ($post->id === Auth::user()->id)
+      {!! Form::open(['url' => '/top','method' => 'post']) !!}
+      <!-- 4.3.1 削除ボタン表示 -->
+        <button class="delete-btn" value="{{ $timeLine -> id }}" onclick="return confirm('このつぶやきを削除します。よろしいでしょうか？')">
+          <img class="delete-icon" src="images/trash.png" alt="削除">
+        </button>
+      {!! Form::close()!!}
+      <!-- 4.3.2 編集ボタン表示 -->
+        <a class="edit-btn" href="" data-target="post-modal-{{ $timeLine -> id }}">
+          <img src="images/edit.png" alt="編集">
+        </a>
+          <!-- 4.4 編集モーダル表示 -->
+            <div class="edit-modal" id="post-modal-{{ $timeLine -> id }}">
+              <div class="modal-contents">
+                {!! Form::open(['url' => '/top','method' => 'post']) !!}
+                {!! Form::hidden('id', $timeLine -> id) !!}
+                {!! Form::text('editPost', $timeLine -> posts, ['required', 'class'=>'post-edit-contents'])!!}
+                  <p class="contents-validator">編集画面が表示されると、選択された投稿内容が初期から入っているように<br>最大200文字までとする</p>
+                    <button class="post-edit-btn">
+                      <img src="images/edit.png" alt="編集モーダル">
+                    </button>
+                {!! Form::close()!!}
               </div>
             </div>
-            @endif
-        </div>
-      </div>
+    @endif
   </div>
 </div>
   @endforeach
+
 @endsection
