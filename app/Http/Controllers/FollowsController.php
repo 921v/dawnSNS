7a56e5afd01371150137d8f20856b7e4d77b47b9
DESKTOP-GUVHBIK\attom <921aaa818@gmail.com>
2023-02-05 18:35:15 +0900

    gettimelineのエラー修正→フォロー・フォロワーの方も修正が必要だった

--- app/Http/Controllers/FollowsController.php
+++ app/Http/Controllers/FollowsController.php
@@ -12,22 +12,22 @@ use App\Follow;
 class FollowsController extends Controller
 {
     //
-    public function followList(Post $post){
+    public function followList(User $user, Post $post){
         $auths = Auth::user();
         $id = Auth::id();
         $follow_ids = Follow::where('follower',$id)->pluck('follow')->toArray();
         $follow_id_lists = User::find($follow_ids);
-        $timeLines = $post->getTimelines($follow_ids);
+        $timeLines = $post->getTimelines($user->id, $follow_ids);
 
         return view('follows.followList',[ 'auths' => $auths , 'follow_id_lists' => $follow_id_lists , 'timeLines' => $timeLines]);
     }
 
-    public function followerList(Post $post){
+    public function followerList(User $user, Post $post){
         $auths = Auth::user();
         $id = Auth::id();
         $follower_ids = Follow::where('follow',$id)->pluck('follower')->toArray();
         $follow_id_lists = User::find($follower_ids);
-        $timeLines = $post->getTimelines($follower_ids);
+        $timeLines = $post->getTimelines($user->id, $follower_ids);
 
         return view('follows.followerList' , [ 'auths' => $auths , 'follow_id_lists' => $follow_id_lists , 'timeLines' => $timeLines]);
     }
